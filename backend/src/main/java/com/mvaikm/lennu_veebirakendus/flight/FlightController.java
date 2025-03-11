6026e32de0c179ee17554199490abd9c3739616d
mvaikm <mvaikm@taltech.ee>
2025-03-11 20:26:19 +0200

    Added flight creation functionality with database support

--- backend/src/main/java/com/mvaikm/lennu_veebirakendus/flight/FlightController.java
+++ backend/src/main/java/com/mvaikm/lennu_veebirakendus/flight/FlightController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 public class FlightController {
     private FlightService flightService;
 
-    public FlightController(FlightService flightService, FlightMapper flightMapper) {
+    public FlightController(FlightService flightService) {
         this.flightService = flightService;
     }
 
